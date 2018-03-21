aaa8dbd7b433dbbf0dd42ed7eb0b70da30fab91a
endovitskayaV <evvEndovitskaya@yandex.ru>
2018-03-21 18:03:50 +0300

    gradle can build war, jar now

--- src/main/java/ru/reksoft/onlineShop/OnlineShopApplication.java
+++ src/main/java/ru/reksoft/onlineShop/OnlineShopApplication.java
@@ -1,13 +1,17 @@
 package ru.reksoft.onlineShop;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@ComponentScan("ru.reksoft.onlineShop")
 @SpringBootApplication
-public class OnlineShopApplication {
+public class OnlineShopApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OnlineShopApplication.class);
+    }
 
     public static void main(String[] args){
         SpringApplication.run(OnlineShopApplication.class, args);
