bde718aeec7c7e307554756729b1094782f8791b
endovitskayaV <evvEndovitskaya@yandex.ru>
2018-04-26 12:16:58 +0300

    WebMvcConfiguration updated

--- src/main/java/ru/reksoft/onlineShop/config/WebMvcConfiguration.java
+++ src/main/java/ru/reksoft/onlineShop/config/WebMvcConfiguration.java
@@ -6,8 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-//TODO: extend WebMvcConfigurer
-public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/ru/reksoft/onlineShop/config/WebSecurityConfig.java
+++ src/main/java/ru/reksoft/onlineShop/config/WebSecurityConfig.java
@@ -22,11 +22,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.customAccessDeniedHandler = customAccessDeniedHandler;
     }
 
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().antMatchers("/src/main/resources/**");
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -39,42 +34,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().accessDeniedHandler(customAccessDeniedHandler);
-
-        //.accessDeniedHandler()
-               // .accessDeniedPage("/accessDenied.jsp");;
-        //http.csrf().disable().authorizeRequests().antMatchers("/login").permitAll(); //.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-        //  http.cors().and().csrf().disable().authorizeRequests().antMatchers("/login").permitAll();
-        //   http.formLogin().loginPage("/login").permitAll().and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-        // http
-//                .authorizeRequests()
-//                .antMatchers("/login", "/items/**").permitAll()
-//                .anyRequest().authenticated();
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-        //.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
-        //.authorizeRequests().antMatchers("/login*").anonymous().anyRequest().permitAll();
-// .cors().and().csrf().disable()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-////                .and()
-////                .headers().disable()
-//               .and()
-//                .authorizeRequests()
-//                .antMatchers("/css/**", "/js/**", "/fonts/**", "/img/**",
-//                        "/login/**", "/signup/**",
-//                        "/basket/**",
-//                        "/", "/items", "items/filter", "items/search", "items/{id}")
-//                .permitAll()
-//                .anyRequest().authenticated();
-
-
-//                .and()
-//                .exceptionHandling().accessDeniedHandler(authEntryPoint);//.authenticationEntryPoint(authEntryPoint);
     }
 
     @Override
